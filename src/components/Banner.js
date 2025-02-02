25a9ff8feab4638ce0293ca6286c76d884318529
Saksham Tiwari <sakshamtiwari157@gmail.com>
2025-02-02 15:21:20 +0530

    Update Banner component with resume link

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -59,8 +59,8 @@ export const Banner = () => {
                 <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                   <span className="tagline">Welcome to my Portfolio</span>
                   <h1>
-                    {`Hi! I'm Saksham`}
-                    <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'>
+                    {`Hi! I'm Saksham `}
+                    <span className="txt-rotate" dataPeriod="1000" data-rotate='[ " Web Developer "]'>
                       <span className="wrap">{text}</span>
                     </span>
                   </h1>
